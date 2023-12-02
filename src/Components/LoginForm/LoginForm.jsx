15c715e2e95fa195da2483630f33409c77ab5ea0
Martin <mmaina290@gmail.com>
2023-12-02 11:22:20 +0300

    Updated wrapper css

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './LoginForm.css'
+import { FaUser, FaLock } from "react-icons/fa";
+
 
 
 function LoginForm() {
@@ -11,10 +13,12 @@ function LoginForm() {
                 <h1>Login</h1>
                 <div className='input-box'>
                     <input type="text" placeholder='Username' required />
+                    <FaUser className='icon'/>
 
                 </div>
                 <div className='input-box'>
                     <input type="password" placeholder='Password' required />
+                    <FaLock className='icon' />
                 </div>
                 <div className="remember-forgot">
                     <label><input type="checkbox" />Remember Me </label>
